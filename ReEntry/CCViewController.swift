bd98fa5432e73dd1c4c1c71b9816e603c8dbac37
Claire Mai <claire.q.mai@gmail.com>
2020-11-22 12:54:42 -0800

    updated community college links

--- ReEntry/CCViewController.swift
+++ ReEntry/CCViewController.swift
@@ -27,8 +27,24 @@ class CCViewController: UIViewController {
             let mapVC = segue.destination as! MHMapViewController
             mapVC.locationName = locationName
         }
+    
+     @IBAction func FederalLoansLink(_ sender: Any) {
+        if let url = URL(string: "https://studentaid.gov/understand-aid/types/loans") {
+            UIApplication.shared.open(url)
+        }
+     }
+    @IBAction func CCInfoLink(_ sender: Any) {
+        if let url = URL(string: "https://bigfuture.collegeboard.org/find-colleges/how-to-find-your-college-fit/community-college-faqs") {
+            UIApplication.shared.open(url)
+        }
+    }
+    @IBAction func OnlineDegreeLink(_ sender: Any) {
+        if let url = URL(string: "https://www.onlineu.com/degrees") {
+            UIApplication.shared.open(url)
+        }
+    }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
