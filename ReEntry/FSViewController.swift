5deac91a6d90095d495c6bbb5576cb133438ddef
Allison Jung <allisonjung@berkeley.edu>
2020-11-22 11:47:31 -0800

    update food shelters

--- ReEntry/FSViewController.swift
+++ ReEntry/FSViewController.swift
@@ -27,6 +27,23 @@ class FSViewController: UIViewController {
             let mapVC = segue.destination as! MHMapViewController
             mapVC.locationName = locationName
         }
+    
+    @IBAction func hungerHotline(_ sender: Any) {
+        if let url = URL(string: "https://www.fns.usda.gov/partnerships/national-hunger-clearinghouse#:~:text=By%20Phone%3A%20Call%20the%20USDA,services%20available%20near%20your%20location") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func foodPantry(_ sender: Any) {
+        if let url = URL(string: "https://www.salvationarmyusa.org/usn/plugins/gdosCenterSearch?start=1") {
+            UIApplication.shared.open(url)
+        }
+    }
+    @IBAction func foodBank(_ sender: Any) {
+        if let url = URL(string: "https://www.feedingamerica.org/find-your-local-foodbank") {
+            UIApplication.shared.open(url)
+        }
+    }
     /*
     // MARK: - Navigation
 
